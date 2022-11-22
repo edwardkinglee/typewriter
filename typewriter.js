59f60fb0de15909f0659e14d2292996fdf8ae9dc
edwardkinglee <edward.lee3@telus.com>
2022-11-22 02:31:41 -0500

    added setTimeout

---
+++ typewriter.js
@@ -0,0 +1,13 @@
+const sentence = "hello there from lighthouse labs";
+let count = 0;
+
+for (const char of sentence) {
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, count);
+  count += 50;
+}
+
+setTimeout(() => {
+  process.stdout.write('\n');
+}, count);
